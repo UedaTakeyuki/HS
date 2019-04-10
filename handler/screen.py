064bdd7d3b9e0049566bec787e021da8c6931246
Takeyuki UEDA <ueda@latelierdueda.com>
2019-04-10 18:07:39 +0900

    reconnect when sokcet is colsed

--- handler/screen.py
+++ handler/screen.py
@@ -37,39 +37,39 @@ class Screen_WSHandler(tornado.websocket.WebSocketHandler):
   route = "/screen_connection/(.*)"
   def open(self, id):
     global connections
-    app_log.info("open: id =  %s", id)
+    app_log.info("S: open: id =  %s", id)
     self.id = id
     connections[id] = {"screen_socket": self}
 
   def on_message(self, message):
     global connections
-    app_log.info("on_message: message =  %s", message)
+    app_log.info("S: on_message: message =  %s", message)
     if self.id in connections:
       if "remote_socket" in connections[self.id]:
         connections[self.id]["remote_socket"].write_message(message)  
  
   def on_close(self):
-    app_log.info("close:")
+    app_log.info("S: close:")
 
 class Remote_WSHandler(tornado.websocket.WebSocketHandler):
   route = "/remote_connection/(.*)"
   def open(self, id):
     global connections
-    app_log.info("open: id =  %s", id)
+    app_log.info("R: open: id =  %s", id)
     self.id = id
     if id in connections:
-      connections[id]["remote_socket"] = self    
+      connections[id]["remote_socket"] = self
 
   def on_message(self, message):
     global connections
-    app_log.info("on_message: message =  %s", message)
+    app_log.info("R: on_message: message =  %s", message)
     if self.id in connections:
       app_log.info("on_message: id =  %s", self.id)
       if "screen_socket" in connections[self.id]:
         connections[self.id]["screen_socket"].write_message(message)  
  
   def on_close(self):
-    app_log.info("close:")
+    app_log.info("R: close:")
 
 class Remote_WebHander(tornado.web.RequestHandler):
   route = "/remote/(.*)"
